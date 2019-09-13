1fd473ba84e00deb6f618e90054b219ee32585fc
yangpengwei <yangpengwei@guazi.com>
2019-09-13 11:12:51 +0800

    增加反射的类

---
+++ dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-consumer/src/main/java/org/apache/dubbo/demo/consumer/proxy0.java
@@ -0,0 +1,34 @@
+package org.apache.dubbo.demo.consumer;
+
+
+import com.alibaba.dubbo.rpc.service.EchoService;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import org.apache.dubbo.common.bytecode.ClassGenerator;
+import org.apache.dubbo.demo.DemoService;
+
+public class proxy0 implements ClassGenerator.DC, EchoService, DemoService {
+    public static Method[] methods;
+    private InvocationHandler handler;
+
+    @Override
+    public String sayHello(String string) {
+        Object[] arrobject = new Object[]{string};
+        Object object = this.handler.invoke(this, methods[0], arrobject);
+        return (String)object;
+    }
+
+    @Override
+    public Object $echo(Object object) {
+        Object[] arrobject = new Object[]{object};
+        Object object2 = this.handler.invoke(this, methods[1], arrobject);
+        return object2;
+    }
+
+    public proxy0() {
+    }
+
+    public proxy0(InvocationHandler invocationHandler) {
+        this.handler = invocationHandler;
+    }
+}
